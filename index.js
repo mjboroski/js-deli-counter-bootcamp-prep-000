06ffde1682a0b8beb2b393921b119e0ee17ca851
Michael Boroski <mjboroski@gmail.com>
2018-04-04 22:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newPerson){
     katzDeliLine.push(newPerson);
-    return "Welcome, "+katzDeliLine[newPerson]+". You are number "+(katzDeliLine.length+1)+" in line.";
+    return "Welcome, "+newPerson+". You are number "+(katzDeliLine.length+1)+" in line.";
 }
