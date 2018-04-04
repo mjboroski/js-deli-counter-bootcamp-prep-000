912539d91b95f9284471e96d2287429ecf867dd4
Michael Boroski <mjboroski@gmail.com>
2018-04-04 22:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newPerson){
     katzDeliLine.push(newPerson);
-    return katzDeliLine[newPerson].value;
+    return katzDeliLine[newPerson].value+1;
 
 }
